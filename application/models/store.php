8ae852ac74af53999ed44f67e2f0dd4400e119ea
Matthew Sainsbury <matthew@sainsbury.za.net>
2013-01-22 16:50:50 +0200

    Started with store images. Very buggy code!

--- application/models/store.php
+++ application/models/store.php
@@ -81,17 +81,17 @@ class Store extends CI_Model
     /// populates all the fields
     function select($storeid)
     {
-        $store=(int)$store;
-        if (!$this->exists($store)) return false;
+        $storeid=(int)$storeid;
+        if (!$this->exists($storeid)) return false;
         
-        $this->db->where('storeid',$store);
+        $this->db->where('storeid',$storeid);
         $query=$this->db->get('stores');
         
         $result=$query->result();
         $result=$result[0];//there should only be one store with a unique id       
         
         //auto-populate fields
-        foreach ($fields as $field)
+        foreach ($this->fields as $field)
             $this->$field=$result->$field?
                             $result->$field : false;
         
@@ -117,6 +117,7 @@ class Store extends CI_Model
             $categories[]=array('categoryid'=>$row->categoryid, 'categoryname'=>$row->text);
         return $categories;
     }
+    
     /// Gets details about all the stores in the mall
     function list_mall($mallid)
     {
@@ -134,4 +135,61 @@ class Store extends CI_Model
             $output[$key]['categories']=$this->categories($output[$key]['storeid']);
         return $output;
     }
+    
+    /// Returns a list of image URLs, their author, and timestamp
+    /// Access token seems needed for facebook batch jobs
+    // TODO: This code is incredible hacky and must be changed. For example, there is almost no error checking
+    function images($token)
+    {
+        if (!$this->storeid)
+            return false;
+        if (!ctype_alnum($token))
+            error('The access token is invalid');
+        
+        $this->storeid=(int)$this->storeid;
+        $query=$this->db->query('SELECT `store-images`.`path`,`store-images`.`userid`, UNIX_TIMESTAMP(`store-images`.`timestamp`) AS `timestamp` FROM `store-images` ORDER BY `store-images`.`timestamp` DESC');
+        
+        $output=array();
+        $userids=array();
+        foreach($query->result() as $row)
+        {
+            $output[]=array('url'=>base_url().'assets/stores/'.$row->path,
+                            'timestamp'=>$row->timestamp, 'userid'=>$row->userid);
+            if (!in_array($row->userid,$userids))
+                $userids[]=$row->userid;
+        }
+        
+        //get everyone's name and uname from facebook all at once
+        $requests='[';//unfortunately we have to construct this JSON object manually. The next few lines of code are UGLY.
+        foreach($userids as $uid)
+            $requests.='{"method":"GET", "relative_url":"'.$uid.'?fields=name,username"}, ';
+        $requests=substr($requests,0,-2).']';
+        
+        // make request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/"); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "access_token=$token&batch=$requests");
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_HTTP200ALIASES, (array)400);
+        curl_setopt($ch, CURLOPT_CAINFO, "c:/xampp/php/ext/cacert.crt");//DEBUG ONLY
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $response = json_decode(curl_exec($ch));
+        curl_close($ch);  
+        
+        foreach ($response as $response_item)
+        {
+            if (ISSET($response_item) && $response_item->code==200) //success
+            {
+                $person=json_decode($response_item->body);
+                
+                foreach($output as $output_key=>$output_item) //for each photo by this user, add their username and name to the photo
+                    if ($output_item['userid']==$person->id)
+                    {
+                        $output[$output_key]['username']=$person->username;
+                        $output[$output_key]['name']=$person->name;
+                    }
+            }
+        }
+        return $output;
+    }       
 }
