d89ec2c949f52a9a294b1bee1b21255df379e951
Matthew Sainsbury <matthew@sainsbury.za.net>
2013-02-02 14:46:36 +0200

    Fleshing out the owner model, and modified hash calculation slightly

--- application/models/owner.php
+++ application/models/owner.php
@@ -1,6 +1,10 @@
 <?php
 class Owner extends CI_Model
 {
+    public $ownerid=false;
+    public $uname=false;
+    public $is_admin=false;
+    
     function __construct()
     {
         parent::__construct();
@@ -20,5 +24,23 @@ class Owner extends CI_Model
         $this->db->where('ownerid',$ownerid);
         $query=$this->db->get('owners');
         return $query->num_rows()>0;
+    }
+    function check_psw($uname,$psw)
+    {
+        //allow only A-Z, dot, hyphen and numbers:
+        $uname=strtolower(preg_replace("/[^a-zA-Z\.\-0-9]",'',$uname)); 
+
+        $this->db->where('uname',$uname);
+        $query=$this->db->get('owners');
+        if ($query->num_rows()!=1)
+            return false;
+        
+        $dbpsw=$query->result()[0];
+        $this->load->helper('bytes');
+        $providedhash=make_hash($dbpsw->salt,$psw);
+        if ($providedhash===$dbpsw->hash)
+            return true;
+        
+        return false;
     }
 }
