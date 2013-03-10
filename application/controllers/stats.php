3c63b87283dfdde20308240bd632561cf88e8ca3
Matthew Sainsbury <matthew@sainsbury.za.net>
2013-03-10 14:55:22 +0200

    how many times a store has been starred

--- application/controllers/stats.php
+++ application/controllers/stats.php
@@ -26,13 +26,30 @@ class Stats extends CI_Controller
     public function stars()
     {
         $this->load->database();
-        $query=$this->db->query('SELECT storeid, count(*) AS stars FROM `store-lists` GROUP BY storeid ORDER BY stars DESC LIMIT 0,20');
-        $output=array();
-        foreach ($query->result() as $row)
+        if (!$this->input->post('storeid'))
         {
-            $output[]=array('storeid'=>$row->storeid, 'stars'=>$row->stars);
+            //No store has been specified, show a leaderboard
+            $query=$this->db->query('SELECT storeid, count(*) AS stars FROM `store-lists` GROUP BY storeid ORDER BY stars DESC LIMIT 0,20');
+            $output=array();
+            foreach ($query->result() as $row)
+            {
+                $output[]=array('storeid'=>$row->storeid, 'stars'=>$row->stars);
+            }
+            send_json($output);
+        } else
+        {
+            //A single store, we should return how many stars that store has
+            $storeid=(int)$this->input->post('storeid');
+            //check for existance
+            $query=$this->db->query('SELECT storeid from `stores` where `storeid`='.$storeid);
+            if ($query->num_rows==0)
+                error('storeid is invalid');
+            //get number of stars
+            $query=$this->db->query('SELECT count(*) as count from `store-lists` where `storeid`='.$storeid);
+            $result=$query->result();
+            $result=$result[0];
+            send_json((int)$result->count);
         }
-        send_json($output);
     }
     /// Checks the health of the API
     /// ASSUMES THERE IS AT LEAST ONE MALL
